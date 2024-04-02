c6d33315e5c2611114039d11f7a63411772f9493
christianbrischetto <christian.brischetto@gmail.com>
2024-04-02 12:00:06 +1100

    new changes bb

--- stepTracker/stepTracker.ino
+++ stepTracker/stepTracker.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal_I2C.h>
 #include <PL_ADXL355.h>
+#include <Stream.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2); //set lcd address and size
 
@@ -11,6 +12,21 @@ int actionPin_out = 4; //CHANGING output pin
 int menuState = 0; //variable for menu cycle
 int stepCount;
 float pace;
+double gx;
+double gy;
+double gz;
+double currentx;
+double currenty;
+double currentz;
+double ref_x;
+double ref_y;
+double ref_z;
+
+// 2V / 3.6V power supply = 195mV/g / 360mV/g output sensitivity
+// 3.3V/3.6V * 0.360mV 
+//                    = 0.33V/g
+double outputSensitivity = 0.33/5 * 1023;
+
 
 void homeScreenDisplay(){
   lcd.clear();
@@ -25,12 +41,12 @@ void selfTestDisplay(){
   lcd.clear();
   lcd.backlight();
   lcd.setCursor(3,0);  
-  lcd.print("Self Test");
+  lcd.print("Calibrate");
 }
 
 bool step(){
   // check if step is taken
-  // return(true);
+  //return(true);
 }
 
 int CountSteps(){ //count steps 
@@ -69,6 +85,53 @@ void walkingpaceDisplay(float pace){
   lcd.print(pace);
 }
 
+void calibrate(){
+  // self test
+  // take self test values as xy 0g, z 1g
+  ref_x = analogRead(A1); 
+  ref_y = analogRead(A0); 
+  ref_z = analogRead(A3); 
+
+  // set ref value as g value
+  printXYZg(0,0,1);
+  
+}
+
+void getXYZg(double& gx, double& gy, double& gz){
+  currentx = analogRead(A1);
+  currenty = analogRead(A0);
+  currentz = analogRead(A3);
+  
+  // 2V / 3.6V power supply = 195mV/g / 360mV/g output sensitivity
+  // 3.3V/3.6V * 360mV 
+  //                    = 330mV/g
+
+  // get difference in current to reference voltage
+  // scale as a percentage of the output sensitivity
+  gx = (currentx - ref_x)/outputSensitivity; 
+  gy = (currenty - ref_y)/outputSensitivity;  
+  gz = (currentz - ref_z)/outputSensitivity;  
+  
+}
+
+void printXYZg(double gx, double gy, double gz){
+  lcd.clear();
+  lcd.backlight();
+  delay(100); //to stop the screen resetting constantly
+
+  lcd.setCursor(0,0);  
+  lcd.print(gx);
+  lcd.print("g");
+
+  lcd.setCursor(8,0);  
+  lcd.print(gy);
+  lcd.print("g");
+
+  lcd.setCursor(0,1);  
+  lcd.print(gz);
+  lcd.print("g");
+}
+
 void setup() {
   lcd.init();
   lcd.clear();
@@ -94,7 +157,7 @@ void loop() {
   //MENU BUTTON
   if (digitalRead(menuPin_in) == LOW){  
     menuState += 1;
-    if (menuState > 3){
+    if (menuState > 4){
       menuState = 0;}
     delay(500);
   }
@@ -116,6 +179,9 @@ void loop() {
     case 3:
       actionPin_out = 4;
       walkingpaceDisplay(pace); break;
+    case 4:
+      getXYZg(gx,gy,gz);
+      printXYZg(gx,gy,gz); break;
   }
 }
 
